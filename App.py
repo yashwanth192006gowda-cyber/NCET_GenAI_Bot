dcb4c4b2e9c893d97a443474c29c282071d7d00f
yashwanth192006gowda-cyber <yashwanth192006gowda@gmail.com>
2026-04-15 11:51:37 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from groq import Groq
+from Groq import Groq
 st.set_page_config("PrapgenAI Content Generator", layout="wide")
 st.title("PrapgenAI - Content Generator")
 st.image("1776062160687.png")
