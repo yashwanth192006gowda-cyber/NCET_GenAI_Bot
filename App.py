11f5c82a1e7b4bc26013d31f313fc596860c562c
yashwanth192006gowda-cyber <yashwanth192006gowda@gmail.com>
2026-04-15 11:35:52 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,3 +3,25 @@ from greg import Greg
 st.set_page_config("PrapgenAI Content Generator", layout="wide")
 st.title("PrapgenAI - Content Generator")
 st.image("1776062160687.png")
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+product = st.text_input("Product")
+audience = st.text_input("Audience")
+if st.button("Generate Content"):
+    prompt = f"Write marketing content for {product} targeting {audience}."
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    st.session_state.text = response.choices[0].message.content
+    text =response.choices[0].message.content
+    st.write(text)
+if "text" in st.session_state:
+    content = st.text_area("Generated Content", st.session_state.text, height=300)
+    st.download_button(
+            label="⬇️ Download as TXT",
+            data=content,
+            file_name="marketing_copy.txt",
+            mime="text/plain"
+        )
+    else:
+        st.info("Generate content first")
