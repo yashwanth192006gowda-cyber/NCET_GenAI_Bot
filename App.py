acd007939d6a6af834c168ad741cd48ba061d78c
yashwanth192006gowda-cyber <yashwanth192006gowda@gmail.com>
2026-04-15 10:40:46 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,3 +2,4 @@ import streamlit as st
 from greg import Greg
 st.set_page_config("PrapgenAI Content Generator", layout="wide")
 st.title("PrapgenAI - Content Generator")
+st.image("1776062160687.png")
